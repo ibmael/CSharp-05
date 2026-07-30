e16bfa6cc115dd03c0d2aa4c414ade241f0ab08a
ibrahim mahmoud elghandour <ibrahimelghandour51@gmail.com>
2026-07-30 14:41:48 +0300

    Task 1: Private field access modifier

--- TASK5ROUTE/Program.cs
+++ TASK5ROUTE/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace TASK5ROUTE
+﻿using static TASK5ROUTE.Books;
+
+namespace TASK5ROUTE
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Task 1
+            Book book1 = new Book();
+            // Console.WriteLine(book1.password);
+            // Error: inaccessible due to protection level
+            Console.WriteLine("Task 1: private field - inaccessible outside class");
+            Console.WriteLine();
+            #endregion
         }
     }
 }
