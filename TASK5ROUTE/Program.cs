3553e6f19378f1512fd11898777497424cc2defb
ibrahim mahmoud elghandour <ibrahimelghandour51@gmail.com>
2026-07-30 14:32:41 +0300

    project initialization

---
+++ TASK5ROUTE/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TASK5ROUTE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
